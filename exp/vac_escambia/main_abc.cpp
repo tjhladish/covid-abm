fbef2903ca08f5f56ad00272f9bd3f55ebc839ad
tjhladish <tjhladish@gmail.com>
2020-08-28 00:40:04 -0400

    Moving pop directories

--- exp/vac_escambia/main_abc.cpp
+++ exp/vac_escambia/main_abc.cpp
@@ -19,7 +19,7 @@ string calculate_process_id(vector<double> &args, string &argstring);
 const string SIM_POP = "escambia";
 //const string SIM_POP = "florida";
 const string HOME_DIR(std::getenv("HOME"));
-const string pop_dir = HOME_DIR + "/work/covid-abm/pop-" + SIM_POP;
+const string pop_dir = HOME_DIR + "/work/covid-abm/pop/" + SIM_POP;
 const string output_dir("/ufrc/longini/tjhladish/");
 //const string imm_dir(output_dir + "");
 
