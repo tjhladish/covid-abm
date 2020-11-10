caf2dbbac17ea9a955bc525757f72f09a17572c8
tjhladish <tjhladish@gmail.com>
2020-11-10 06:34:02 -0500

    Heavy refactoring of gleam data script

--- pop/florida/gleam_scrape.R
+++ pop/florida/gleam_scrape.R
@@ -1,52 +1,59 @@
 library(data.table)
 
-d = as.numeric(readLines('gleam_cci-raw.csv'))
-m = matrix(d, ncol=2, byrow=T)
-cci_dt = data.table(m)
-names(cci_dt) = c('x', 'y')
-cci_min = 0.1765
-cci_max = 1.7658
-y_min = min(cci_dt$y)
-y_max = max(cci_dt$y)
-cci_dt$cci = (1 - (cci_dt$y - y_min)/(y_max-y_min))*(cci_max - cci_min) + cci_min
+# gleam_cmi-raw.csv    Cuebiq mobility index
+# gleam_cuci-raw.csv   Cuebiq unique contact index
+# gleam_ccdi-raw.csv   Cuebiq contact duration index
 
-d2 = as.numeric(readLines('gleam_cmi-raw.csv'))
-m2 = matrix(d2, ncol=2, byrow=T)
-cmi_dt = data.table(m2)
-names(cmi_dt) = c('x', 'y')
-cmi_min = 0.2488 
-cmi_max = 1.2985
-y_min = min(cmi_dt$y)
-y_max = max(cmi_dt$y)
-cmi_dt$cmi = (1 - (cmi_dt$y - y_min)/(y_max-y_min))*(cmi_max - cmi_min) + cmi_min
+DATE_FIRST = as.Date('2020-02-01') # first day in timeseries
+DATE_LAST  = as.Date('2020-10-25') # last day in timeseries
+
+process_gleam = function(filename, metric_name, metric_min, metric_max, date_min, date_max) {
+    d = as.numeric(readLines(filename))
+    m = matrix(d, ncol=2, byrow=T)
+    dt = data.table(m)
+    names(dt) = c('x', 'y')
+    #cci_min = 0.1654
+    #cci_max = 1.3195
+    y_min = min(dt$y)
+    y_max = max(dt$y)
+    dt[,metric_name] = (1 - (dt$y - y_min)/(y_max-y_min))*(metric_max - metric_min) + metric_min
+
+    x_min = min(dt$x)
+    x_max = max(dt$x)
+    dt$date = (dt$x - x_min)/(x_max-x_min)*(date_max - date_min) + date_min
+
+    stopifnot(nrow(dt) %% 3 == 1) # We're expecting three values for each day, except the last one, which we expect to have one
+    dt$date2 = c(rep(seq(DATE_FIRST, DATE_LAST - 1, by="day"), each = 3), DATE_LAST)
+    model_daily = dt[, .(metric_name = mean(get(metric_name))), by=.(date=date2)]
+    max_rel_metric = max(model_daily[which(model_daily$date >= as.Date('2020-03-01')),2])
+
+    adj_name = paste0('adj_', metric_name)
+    setnames(model_daily, "metric_name", metric_name)
+    model_daily[,adj_name] = model_daily[,metric_name, with=F]/max_rel_metric # this "with" stuff is some serious bullshit
+    print(model_daily)
+    model_daily[,(metric_name):=NULL]
+
+    return(model_daily);
+}
 
 date_min = as.POSIXct("2020-02-01")
 date_max = as.POSIXct("2020-10-25")
 
-x_min = min(cci_dt$x)
-x_max = max(cci_dt$x)
-cci_dt$date = (cci_dt$x - x_min)/(x_max-x_min)*(date_max - date_min) + date_min
-cci_dt$date2 = c(rep(seq(as.Date("2020-02-01"), as.Date("2020-10-24"), by="day"), each = 3), as.Date("2020-10-25"))
-model_cci = cci_dt[, .(cci = mean(cci)), by=.(date=date2)]
-max_rel_cci = max(model_cci[which(model_cci$date >= as.Date('2020-03-01')),2])
-model_cci$adjusted_cci = model_cci$cci/max_rel_cci
-#model_cci$adjusted_cci[which(model_cci$date < as.Date('2020-03-10'))] = 1.0
-model_cci$adjusted_cci[which(model_cci$date < as.Date('2020-03-10'))] = as.numeric(model_cci[date == as.Date('2020-03-10'),'adjusted_cci'])
+cmi_dt  = process_gleam('gleam_cmi-raw.csv',  'cmi',  metric_min = 0.255, metric_max = 1.315, date_min, date_max)
+cuci_dt = process_gleam('gleam_cuci-raw.csv', 'cuci', metric_min = 0.387, metric_max = 3.314, date_min, date_max)
+ccdi_dt = process_gleam('gleam_ccdi-raw.csv', 'ccdi', metric_min = 0.314, metric_max = 3.431, date_min, date_max)
 
-x_min = min(cmi_dt$x)
-x_max = max(cmi_dt$x)
-cmi_dt$date = (cmi_dt$x - x_min)/(x_max-x_min)*(date_max - date_min) + date_min
-cmi_dt$date2 = c(rep(seq(as.Date("2020-02-01"), as.Date("2020-10-24"), by="day"), each = 3), as.Date("2020-10-25"))
-model_cmi = cmi_dt[, .(cmi = mean(cmi)), by=.(date=date2)]
-max_rel_cmi = max(model_cmi[which(model_cmi$date >= as.Date('2020-03-01')),2])
-model_cmi$adjusted_cmi = model_cmi$cmi/max_rel_cmi
-model_cmi$adjusted_cmi[which(model_cmi$date < as.Date('2020-03-10'))] = as.numeric(model_cmi[date == as.Date('2020-03-10'),'adjusted_cmi'])
+#cci_min = 0.1765 # values for number of contacts metric
+#cci_max = 1.7658
+#cci_min = 0.1654
+#cci_max = 1.3195
 
-model = data.table(date = seq(as.Date("2020-01-01"), as.Date("2020-10-25"), by="day"), rs_cci_cmi = as.numeric(NA))
-model = merge(model, model_cci, by='date', all.x=T)
-model = merge(model, model_cmi, by='date', all.x=T)
-model$rs_cci_cmi = sqrt(model$adjusted_cci * model$adjusted_cmi)
-model$rs_cci_cmi = model$rs_cci_cmi/max(model$rs_cci_cmi, na.rm=T)
-model$rs_cci_cmi[is.na(model$rs_cci_cmi)] = 1.0
+model = data.table(date = seq(as.Date("2020-01-01"), DATE_LAST, by="day"), compound = as.numeric(NA))
+model = merge(model, cmi_dt,  by='date', all.x=T)
+model = merge(model, cuci_dt, by='date', all.x=T)
+model = merge(model, ccdi_dt, by='date', all.x=T)
+model$compound = (model$adj_cmi * model$adj_cuci * model$adj_ccdi)**(1/3)
+#model$compound = model$compound/max(model$compound, na.rm=T)
+model$compound[is.na(model$compound)] = model$compound[model$date == as.Date('2020-02-01')]
 
-#write.csv(model[,c(1,3,2)], "gleam_cci_cmi-adjusted.csv", quote=F, row.names=F)
+write.csv(model, "gleam_metrics.csv", quote=F, row.names=F)
