a2345ed6bde2c0f52c04101998133e80d8086eba
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2021-09-12 12:38:49 +0100

    adds plots for relative impact of variant (with and without vaccine) and relative impact of vaccine (with and without variant)

--- exp/vac_impact/variant-plots.R
+++ exp/vac_impact/variant-plots.R
@@ -14,13 +14,38 @@ mlt <- readRDS(.args[1])
 novariant <- mlt[mutation == 0][, -c("serial", "seed", "mutation", "variable", "week")]
 variant <- mlt[mutation == 1][, -c("serial", "seed", "mutation", "variable", "week")]
 
-var.impact <- variant[novariant, on = .(realization, var, date, vac)]
-var.impact
+var.impact <- variant[
+  date >= "2021-01-01" & var != "Rt"
+][novariant, on = .(realization, var, date, vac), nomatch = 0]
+var.impact[order(date), cvalue := cumsum(value), by=.(realization, vac, var) ]
+var.impact[order(date), i.cvalue := cumsum(i.value), by=.(realization, vac, var) ]
 
-ggplot(impact) + aes(date, value,
-  color = linetype=c("NONE","VAC")[vac+1],
-  group = interaction(serial,realization,vac,mutation)
+# how much excess does variant cause?
+var.impact[order(date), excess := value - i.value, by=.(realization, vac, var) ]
+var.impact[order(date), cexcess := cvalue - i.cvalue, by=.(realization, vac, var) ]
+# what is relative impact?
+var.impact[order(date), ceff := cexcess/i.cvalue, by=.(realization, vac, var) ]
+
+p <- ggplot(var.impact) + aes(
+  date, ceff, color = vac,
+  group = interaction(realization, vac)
 ) +
-  facet_grid(var ~ ., scales = "free") +
+  facet_grid(var ~ ., scales = "free", labeller = labeller(
+    var = c(c="Cases",d="Deaths")
+  )) +
   geom_line(alpha = 0.1) +
-  theme_minimal()
+  scale_x_date(
+    name=NULL,
+    date_breaks = "month", minor_breaks = NULL,
+    date_labels = "%b %y"
+  ) +
+  scale_y_continuous("Relative Impact of Variant Introduction") +
+  scale_color_continuous(
+    "Vaccine", breaks = c(0,1), labels = c("None", "Some"),
+    guide = guide_legend(override.aes = list(alpha=1))
+  ) + theme_minimal() + theme(
+    legend.position = c(0+0.05, 1-0.05),
+    legend.justification = c(0, 1)
+  )
+
+ggsave(tail(.args, 1), p, width = 7, height = 4, units = "in", dpi = 300)
