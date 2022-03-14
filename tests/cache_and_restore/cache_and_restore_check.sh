5a9d2f79263ff532a55e4e33466eba1e25ca7a5b
pillai-alexander <pillai.alexander.n@gmail.com>
2022-03-14 09:18:10 -0400

    Adding script to run the test and output results

---
+++ tests/cache_and_restore/cache_and_restore_check.sh
@@ -0,0 +1,16 @@
+make sim_test
+
+time ./sim_test abc_covid.json --simulate --serial 0
+
+CKSUM=`cksum plot_log_*`
+NUM_MATCH=`cksum plot_log_* | cut -d' ' -f1,2 | uniq -c | wc -l`
+
+if [[ $NUM_MATCH -eq 1 ]]
+then
+    echo "CACHE AND RESTORE TEST PASSED"
+    echo "ALL CHECKSUMS EQUIVALENT"
+    echo "$CKSUM"
+else
+    echo "CACHE AND RESTORE TEST FAILED"
+    echo "$CKSUM"
+fi
