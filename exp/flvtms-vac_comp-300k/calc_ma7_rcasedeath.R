e9f2e323268a761b73f733b4098c53bd74f47028
pillai-alexander <pillai.alexander.n@gmail.com>
2022-07-01 09:45:43 -0400

    Adding script to calculate 7-day moving avg of rep. cases
    For use in PPB fitting. Smoothing reported cases may improve fitting of 
    the omicron wave.
    
    Use: Rscript calc_ma7_rcasedeath.R <link to rcasedeath file>

---
+++ exp/flvtms-vac_comp-300k/calc_ma7_rcasedeath.R
@@ -0,0 +1,18 @@
+if (interactive()) { setwd("~/documents/work/covid-abm/exp/flvtms-vac_comp-300k/") }
+
+.args <- if (interactive()) c(
+  "rcasedeath-florida.csv"
+) else commandArgs(trailingOnly = TRUE)
+
+ed = read.csv(.args[1])
+ed$date = as.Date(ed$Date)
+
+ed$crcase  = cumsum(ed$rcase)
+ed$crdeath = cumsum(ed$rdeath)
+
+ed$rcase_ma7 = filter(c(0,0,0,ed$rcase), rep(1/7, 7), sides = 2)[-(1:3)]
+ed$crcase_ma7 = cumsum(ed$rcase_ma7)
+
+out = data.frame(Date = ed$Date, rcase_ma7 = ed$rcase_ma7, rdeath = ed$rdeath, death_incd = ed$death_incd, excess = ed$excess)
+out = out[1:(nrow(out)-3),]
+write.csv(out, "rcasedeath-florida_ma7cases.csv", quote = F, row.names = F)
