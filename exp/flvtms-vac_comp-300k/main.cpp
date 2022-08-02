42ca21368b02f433841c35ae36bc859c93c3dcf6
pillai-alexander <pillai.alexander.n@gmail.com>
2022-08-02 12:28:06 -0400

    Adding par->vaccineInfConstraint to main so it can be changed.

--- exp/flvtms-vac_comp-300k/main.cpp
+++ exp/flvtms-vac_comp-300k/main.cpp
@@ -603,6 +603,8 @@ vector<double> simulator(vector<double> args, const unsigned long int rng_seed, 
         vector<int> min_ages(par->runLength, 5);
         vc->set_min_age(min_ages);       // needed for e.g. urgent vaccinations
 
+        par->vaccineInfConstraint = VACCINATE_ALL_INF_STATUSES;
+
         cerr << "Vaccination scenario:\n"
              << GENERAL_CAMPAIGN << "\n"
                                  << "\tduration " << vc->get_start_of_campaign(GENERAL_CAMPAIGN) << "--" << vc->get_end_of_campaign(GENERAL_CAMPAIGN) << "\n"
