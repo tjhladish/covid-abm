99d31bb941318c0e15e3799566ee4c37fd2737df
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2022-10-07 07:40:25 +0100

    update cumulative doses vis

--- exp/active-vac/fig/cumulative_doses.R
+++ exp/active-vac/fig/cumulative_doses.R
@@ -7,7 +7,6 @@ stopifnot(all(sapply(.pkgs, require, character.only = TRUE)))
 .args <- if (interactive()) c(
   file.path("fig", "vis_support.rda"),
   file.path("fig", "process", c("digest-doses.rds", "digest-key.rds", "alt_eff.rds")),
-  file.path(c("covax_doses_COVAX_only.txt", "covax_doses_MIC_only.txt")),
   file.path("fig", "output", "cumulative_doses.png")
 ) else commandArgs(trailingOnly = TRUE)
 
@@ -18,17 +17,13 @@ load(.args[1])
 tar <- "doses"
 
 scn.dt <- readRDS(.args[3])
-addscn <- scn.dt[act_alloc == "none" & !(pas_alloc %in% c("none", "FL"))]
-scns <- c(
-  readRDS(.args[4])[, unique(scenario)],
-  scn.dt[act_alloc == "none" & !(pas_alloc %in% c("none", "FL")), unique(scenario)]
-)
+scns <- scn.dt[!(pas_alloc == "none" & act_alloc == "none"), unique(scenario)]
 
 #' comes key'd
 doses.dt <- readRDS(.args[2])[
   eval(datefilter)
 ][scenario %in% scns, .(
-  scenario, realization, date, c.value
+  scenario, realization, outcome, date, c.value
 )]
 
 intscns <- doses.dt[, unique(scenario)]
@@ -41,37 +36,31 @@ plt.dt <- setkeyv(
 rm(doses.dt)
 gc()
 
-plt.dt[, talloc := factor(
+plt.qs <- quantile(
+  plt.dt,
+  j = .(c.value), sampleby = "realization",
+  probs = qprobs(c(`90`=.9), mid = TRUE, extent = FALSE)
+)[, talloc := factor(
   fifelse(
-    pas_alloc == "none", as.character(act_alloc), fifelse(
-    pas_alloc %in% c("FL", "FL+ring"), "FL+", as.character(pas_alloc)
-  )), levels = c("COVAX", "MIC", "FL+"), ordered = TRUE
+    pas_alloc == "none",
+    as.character(act_alloc), as.character(pas_alloc)
+  ), levels = c("LIC", "MIC", "HIC", "USA"), ordered = TRUE
 )][, qfac := factor(c("No Additional NPI", "Quarantine Contacts")[quar+1]) ]
 
-p <- ggplot(plt.dt) + aes(
-  x = date, y = c.value,
+p <- ggplot(plt.qs) + aes(
+  x = date,
   color = act_vac,
-  linetype = factor(c("unconditional", "conditional")[inf_con+1]),
-  sample = realization
+  linetype = factor(c("unconditional", "conditional")[inf_con+1])
 ) +
   facet_nested(rows = vars(qfac), cols = vars(talloc)) +
   geom_month_background(
-    plt.dt, by = c("qfac", "talloc"),
-    font.size = 3, value.col = "c.value"
-    # ,
-    # ymax = plt.dt[, max(c.value), by=.(talloc)],
-    # ymin = plt.dt[, min(c.value), by=.(talloc)]
+    plt.qs, by = c("qfac", "talloc"),
+    font.size = 3, value.col = "qmed",
+    ymax = plt.qs[, max(q90h)],
+    ymin = plt.qs[, min(q90l)]
   ) +
-  stat_spaghetti(
-    aes(alpha = after_stat(sampleN^-1))
-  ) +
-#  geom_hline(aes(yintercept=0, color = "none")) +
-  scale_y_continuous(
-    name = sprintf(
-      "Cumulative Vaccination"
-    )
-  ) +
-  scale_x_null() +
+  geom_ribbon(aes(ymin=q90l, ymax=q90h, fill=act_vac, color=NULL), alpha=0.25) +
+  geom_line(aes(y=qmed)) +
   scale_color_discrete(
     "Vaccine Program",
     breaks = c("risk", "ring", "none"),
@@ -79,7 +68,15 @@ p <- ggplot(plt.dt) + aes(
       ring="Ring Vaccination",
       risk="Risk-Based Strategy",
       none="Standard Only"
-    )) +
+    ),
+    aesthetics = c("color", "fill")
+  ) +
+  scale_y_continuous(
+    name = sprintf(
+      "Cumulative Vaccination"
+    )
+  ) +
+  scale_x_null() +
   scale_linetype_manual("Vaccinate...", labels = c(conditional="Condtional on\nCase History", unconditional = "Unconditionally"), values = c(conditional="dashed", unconditional="solid")) +
   scale_alpha(range = c(0.01, 1)) +
   theme_minimal() +
