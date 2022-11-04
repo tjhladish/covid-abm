eb9a7abc7292f50396bfe50ff1af5a2f7f352448
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2022-11-04 22:23:25 +0000

    inf risk

--- exp/active-vac/fig/test_fig.R
+++ exp/active-vac/fig/test_fig.R
@@ -1,53 +1,68 @@
 
-.pkgs <- c("data.table", "ggplot2", "cabputils")
+.pkgs <- c("data.table", "ggplot2", "scales", "cabputils")
 .pkgs |> sapply(require, character.only = TRUE) |> all() |> stopifnot()
 
 .args <- commandArgs(args = c(
   "~/Downloads", # TODO some digest of this output
   file.path("~", "Downloads", "serial_lookup.csv"), # TODO extract
   file.path("fig", "vis_support.rda"),
-  "something.png"
+  file.path("fig", "output", "inf_risk.png")
 ))
 
 dt <- .args[1] |> list.files(pattern = "vpdr.*csv$", full.names = TRUE) |>
-  lapply(fread) |> rbindlist() |> melt.data.table(id.vars = c("serial", "day"))
+  setNames(nm = _) |>
+  lapply(fread, drop = 1) |> rbindlist(idcol = "file") |>
+  melt.data.table(id.vars = c("day", "file")) |>
+  DT(, serial := gsub("^.*_(\\d+)\\.csv$", "\\1", file) |> as.integer())
+
+dt$file <- NULL
 
 serialkey <- .args[2] |> fread()
 .args[3] |> load()
 
 translator <- dt[, serialkey[between(serial, `min(serial)`, `max(serial)`), .(quar, pas_vac, act_vac, pas_alloc, act_alloc, inf_con)], by=serial]
 
-dt[translator, on=.(serial), vtype := c("none", "ring", "risk", "age")[act_vac+1] ]
+plt.dt <- dt[translator, on=.(serial), .(
+  date = day + as.Date("2020-02-10"),
+  variable, value,
+     vtype = factor(c("none", "ring", "risk", "age")[act_vac+1]),
+     alloc = factor(
+       c("none", "LIC", "MIC", "HIC", "USA")[ifelse(pas_vac == 0, act_alloc, pas_alloc)+1],
+       levels = c("none", "LIC", "MIC", "HIC", "USA"), ordered = TRUE
+     ),
+     quar,
+     inf_con
+   )
+][inf_con != 2]
 
-dt[variable == "inf_risk", variable := "infrisk_tot"]
-dt[variable %like% "inf_risk", variable := gsub("inf_risk", "infrisk", variable) ]
+plt.dt[variable == "inf_risk", variable := "infrisk_tot"]
+plt.dt[variable %like% "inf_risk", variable := gsub("inf_risk", "infrisk", variable) ]
 
-
-plt.dt <- dt[
+plt.dt <- plt.dt[
   variable %like% "infrisk"
 ][, var := gsub("infrisk_", "", variable) |> factor(levels = c("nat", "vax", "tot"), ordered = TRUE) ]
 
 p2 <- ggplot(plt.dt) +
-  aes(x = day + as.Date("2020-02-10"), y = value, color = vtype) +
+  aes(x = date, y = value, color = vtype, linetype = factor(c("nonpi", "wquar")[quar+1])) +
   facet_grid(
-    var ~ ., switch = "y",
+    var ~ alloc, switch = "y",
     labeller = labeller(var = c(
-      nat = "Infection Derived",
-      vax = "Vaccine Derived",
+      nat = "Prior Infection",
+      vax = "Vaccination",
       tot = "All Sources"
     ))
   ) +
-  geom_month_background() +
+  geom_month_background(
+    plt.dt, by = c(row="var", col="alloc"), font.size = 3
+  ) +
   geom_line() +
   theme_minimal() + theme(
     legend.position = "bottom", strip.placement = "outside"
   ) +
-  scale_y_continuous(
-
-  ) +
+  scale_y_continuous(name = "Mean Infection Risk Given Exposure,\nReduced Due to ...") +
   scale_x_null() +
   scale_color_strategy(
     guide = guide_legend(title.position = "top", title.hjust = 0.5)
-  )
+  ) + scale_linetype_quar()
 
-store(.args, p, width = 10, height = 6, bg = "white")
+store(.args, p2, width = 10, height = 6, bg = "white")
